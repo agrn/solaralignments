772d5fb5b68ab91dea13cdcbe39cf0340826e653
Alban Gruin <alban@pa1ch.fr>
2025-10-01 23:43:27 +0200

    apparent: add a function to compute the altitude of a mountain
    This adds a new fuction to compute the altitude of a mountain above the
    astronomical horizon.  This solution takes atmospheric refraction into
    account.  It will be used to compute automatically the highest and
    lowest visual altitude bounds for a given distance to the target mountain.
    
    Signed-off-by: Alban Gruin <alban@pa1ch.fr>

--- src/apparent.ml
+++ src/apparent.ml
@@ -1,6 +1,16 @@
 (* (C) 2025 Alban Gruin, released under LGPL 3 *)
 
+open Float
+
 open Helpers
+
+(* https://aty.sdsu.edu/explain/atmos_refr/altitudes.html *)
+let apparent_height ho hm dist r =
+  let k = 35. /. 150. in
+  let h = (dist *. dist) *. (1. -. k) /. (2. *. r) in
+  let h' = ho +. h in
+  let altrad = atan2 (hm -. h') dist in
+  rad2deg altrad
 
 let bennett_refr h =
   (* Jean Meeus, Astronomical Algorithms, Formule 15.4 *)
--- src/apparent.mli
+++ src/apparent.mli
@@ -1,3 +1,4 @@
 (* (C) 2025 Alban Gruin, released under LGPL 3 *)
 
+val apparent_height : float -> float -> float -> float -> float
 val bennett_refr : float -> float
